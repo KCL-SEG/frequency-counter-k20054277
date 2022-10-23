9c6890d4f7787870aebfb8d8ae2da3e858335d0f
jiaqi xu <jiaqi.3.xu@kcl.ac.uk>
2022-10-23 23:25:09 +0100

    complete

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,13 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
     # Your code goes here
+    frequencies = {}
+    for i in items:
+        k = str(i)
+        if k in frequencies :
+            frequencies[k] += 1 
+        else:
+            frequencies[k] = 1
+    
     return frequencies
